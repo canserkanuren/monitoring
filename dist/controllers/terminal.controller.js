27b59f7e98005c9b69d2aa95052c3c8b8ac879c8
Jordan Nijean <onza1101@gmail.com>
2018-05-21 16:24:07 +0200

    Removed dist folder

--- dist/controllers/terminal.controller.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var router = express_1.Router();
-router.get('/', function (req, res) {
-    res.render('terminal', { title: 'Terminal', message: 'Cancer !' });
-});
-exports.TerminalController = router;
-//# sourceMappingURL=terminal.controller.js.map
--- dist/terminalHelper.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var xterm_1 = require("xterm");
-var xterm = new xterm_1.Terminal();
-xterm.fit(xterm);
-//# sourceMappingURL=terminalHelper.js.map
